142066cc78cfa6f61d79ed281982dda886548c81
nuelSBN <echinedu007@gmail.com>
2021-10-05 07:41:31 +0100

    banner section completed

--- src/components/homepage/Body/BannerSection/BannerSection.jsx
+++ src/components/homepage/Body/BannerSection/BannerSection.jsx
@@ -4,18 +4,19 @@ import "./bannerSection.css";
 function BannerSection() {
   return (
     <div className="banner">
-      <div className="leftBanner">
-        <div className="bannerTextContainer">
-          <h1 className="bannerText">
-            Browse the Top <span>Giovany Homes</span>
-          </h1>
-          <p className="bannerTextP">
-            <span className="color">Giovany Homes</span> is a modular home
-            marketplace that helps you find, compare, and purchase a{" "}
-            <span className="break">prefab modular home.</span>
-          </p>
-        </div>
+      <div className="bannerTextContainer">
+        <h1 className="bannerText">
+          Browse the Top <span>Giovany Homes</span>
+        </h1>
+        <p className="bannerTextP">
+          <span className="color">Giovany Homes</span> is a modular home
+          marketplace that helps you find, compare, and purchase
+          <span className="break"> a prefab modular home.</span>
+        </p>
       </div>
+      <button>
+        <span>BROWSE PROPERTIES NOW</span>
+      </button>
     </div>
   );
 }
